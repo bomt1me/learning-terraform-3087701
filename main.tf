02dbdbfdac1c111f57bfe1bcc23ace5af74ff6e9
Calvin Spring <calvin@cardiologs.com>
2023-10-26 15:12:57 -0400

    feat: Add autoscaling.

--- main.tf
+++ main.tf
@@ -33,19 +33,23 @@ module "web_vpc" {
   }
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [module.web_sg.security_group_id]
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.1.0"
 
-  subnet_id = module.web_vpc.public_subnets[0]
+  name     = "web"
+  min_size = 1
+  max_size = 2
 
-  tags = {
-    Name = "HelloWorld"
-  }
+  vpc_zone_identifier = module.web_vpc.public_subnets
+  target_group_arns   = module.web_alb.target_group_arns
+  security_groups     = [module.web_sg.security_group_id]
+
+  image_id            = data.aws_ami.app_ami.id
+  instance_type       = var.instance_type
 }
 
-module "alb" {
+module "web_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
@@ -63,12 +67,6 @@ module "alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.web.id
-          port = 80
-        }
-      }
     }
   ]
 
